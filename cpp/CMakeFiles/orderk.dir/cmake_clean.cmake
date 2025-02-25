3136955714e0b80e3bdd353b59a1a90d55b52d2e
Armando Albornoz <armando.a1@ufl.edu>
2025-02-25 17:21:25 -0500

    =first commit

---
+++ cpp/CMakeFiles/orderk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orderk.dir/src/main.cpp.o"
+  "CMakeFiles/orderk.dir/src/main.cpp.o.d"
+  "orderk"
+  "orderk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orderk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
